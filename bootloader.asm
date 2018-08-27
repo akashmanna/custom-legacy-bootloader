2603b3b64f79681dff03b5f26d4627df4b60d432
Akash Manna <akashmanna@gmail.com>
2018-08-27 16:51:40 +0530

    added gdt table structure

--- bootloader.asm
+++ bootloader.asm
@@ -3,24 +3,27 @@ org 0x7c00
 
 boot:
 	mov ax, 0x2401
-	int 0x15 ; A20 bit
+	int 0x15 								; A20 bit
 	mov ax, 0x3
-	int 0x10 ; VGA - text mode
+	int 0x10 								; VGA - text mode
 	cli
 	lgdt [gdt_pointer]
 	mov eax, cr0
 	or eax,0x1
 	mov cr0, eax
 	jmp CODE_SEG:boot2
+
+
+;GDT Declarations
 gdt_start:
 	dq 0x0
 gdt_code:
-	dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10011010b
-	db 11001111b
-	db 0x0
+	dw 0xFFFF			; LIMIT - LOW
+	dw 0x0 				; BASE - LOW
+	db 0x0 				; BASE - MID
+	db 10011010b  		; ACCESS BITS
+	db 11001111b 		; | FLAGS | LIMIT - HIGH |
+	db 0x0 				; BASE - HIGH
 gdt_data:
 	dw 0xFFFF
 	dw 0x0
@@ -44,20 +47,23 @@ boot2:
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
-	mov esi,hello
-	mov ebx,0xb8000 ; VGA Text Buffer addr
-.loop:
+	mov esi,text
+	mov ebx,0xb8000 							; VGA Text Buffer addr
+
+.println:
 	lodsb
 	or al,al
 	jz halt
-	or eax,0x0400 ; | BG | FG | CHAR | , 0000 - Blk, 0100 - Blue, 0F00 - White
+	or eax,0x0400 								; | BG | FG | CHAR | , 0000 - Blk, 0100 - Blue, ... 0F00 - White
 	mov word [ebx], ax
 	add ebx,2
-	jmp .loop
+	jmp .println
+
 halt:
 	cli
 	hlt
-hello: db "Hello from the protected side!",0
+
+text: db "Hello from the protected side!",0
 
 times 510 - ($-$$) db 0
 dw 0xaa55
